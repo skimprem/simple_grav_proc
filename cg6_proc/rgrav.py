c119337dabda741c2e5f473a47131d94c121b3df
skimprem <skimprem@gmail.com>
2023-06-08 15:57:20 +0600

    Add windows select data file

--- cg6_proc/rgrav.py
+++ cg6_proc/rgrav.py
@@ -1,26 +1,37 @@
 import argparse
 import os
 from cg6_proc.utils import cg6_data_file_reader, get_readings, get_ties, get_mean_ties, get_ties_sum, print_means, make_vgfit_input, make_output
+from tkinter import filedialog as fd
+import sys
 
-parser = argparse.ArgumentParser(
+to_vgfit = False
+v = False
+
+if sys.platform.startswith('win32'):
+    data_file = fd.askopenfilename()
+elif sys.platform.startswith('linux'):
+    parser = argparse.ArgumentParser(
                     prog='rgrav',
                     description='Read CG-6 data file and compute ties',
                     epilog='This program read CG-6 data file, then compute ties by ...')
 
-parser.add_argument('data_file')                  # positional argument
-parser.add_argument('-v', action='store_true')  # on/off flag
-parser.add_argument('--to_vgfit', action='store_true')  # on/off flag
+    parser.add_argument('data_file')                  # positional argument
+    parser.add_argument('-v', action='store_true')  # on/off flag
+    parser.add_argument('--to_vgfit', action='store_true')  # on/off flag
+    args = parser.parse_args()
+    data_file = args.data_file
+    args.to_vgfit = to_vgfit
+    args.v = v
 
-args = parser.parse_args()
+data = cg6_data_file_reader(data_file) 
 
-data = cg6_data_file_reader(args.data_file) 
 readings = get_readings(data)
 ties = get_ties(readings)
 means = get_mean_ties(ties)
 sum = get_ties_sum(means)
 
-basename = os.path.splitext(os.path.basename(args.data_file))[0]
-dirname = os.path.dirname(args.data_file)
+basename = os.path.splitext(os.path.basename(data_file))[0]
+dirname = os.path.dirname(data_file)
 
 output_file = 'report_'+basename+'.txt'
 make_output(means, output_file)
@@ -28,9 +39,9 @@ report = open(output_file, 'a')
 report.write(f'\n Sum of the ties = {get_ties_sum(means): .2f}\n')
 report.close()
 
-if args.to_vgfit:
+if to_vgfit:
     make_vgfit_input(means, basename+'.csv')
 
-if args.v:
+if v:
     print_means(means)
     print(f'Sum of the ties = {sum: .2f}')
